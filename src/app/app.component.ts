bfde7fa50afd6f406ffb39fcdbae7cc2e90b0bbc
Akiah <akilahlittlejohn@gmail.com>
2023-03-01 14:48:50 -0600

    remove app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TableComponent } from './table/table.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone:true,
+  imports:[TableComponent, CommonModule]
 })
 export class AppComponent {
   title = 'book-list';
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
-/// <reference types="@angular/localize" />
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent)
