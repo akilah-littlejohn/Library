f589fe3d1e14ef5bc11b8b0eea5ba8665197df9a
Akiah <akilahlittlejohn@gmail.com>
2023-03-01 16:11:45 -0600

    build form for modal

---
+++ src/app/book-form/book-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-book-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './book-form.component.html',
+  styleUrls: ['./book-form.component.scss']
+})
+export class BookFormComponent {
+
+}
