3b35921f6cfdea2f11564ea0430935309c5caf24
Akiah <akilahlittlejohn@gmail.com>
2023-03-02 07:01:58 -0600

    define Book storage

--- src/app/library.service.ts
+++ src/app/library.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
+import { BookDetails } from './book-details';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibraryService {
-  books:[] = []
+  books:Book[] = []
 
-  constructor(public bookType:Book) { }
+  constructor(public bookType: Book) { }
 
-  addBook(){
-
+  addBook(val:BookDetails):void {
+    const bookInfo = new Book(val.author, val.title, val.pages)
+    this.books.push(bookInfo)
   }
 
-  updateBook(){
-  }
+  updateBook() { }
 
-  deleteBook(){}
+  deleteBook() { }
 
-  clearLibrary(){}
+  clearLibrary() { }
 }
