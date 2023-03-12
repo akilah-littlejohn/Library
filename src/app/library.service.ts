1cc8e0263941a6a1089f39c25908a52c01193540
Akiah <akilahlittlejohn@gmail.com>
2023-03-11 20:53:58 -0600

    feat crud logic in service

--- src/app/library.service.ts
+++ src/app/library.service.ts
@@ -2,28 +2,37 @@ import { Injectable } from '@angular/core';
 import { BookDetails } from './book-details';
 
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibraryService {
-  api: string = 'http://localhost:3000/books'
-  booksId: number = 0
+  api: string = 'http://localhost:3000/books';
 
-  constructor( private http: HttpClient) { }
+
+  constructor(private http: HttpClient) { }
 
   getBook() {
     return this.http.get<BookDetails[]>(this.api)
-   }
-
-  addBook(val: BookDetails): void {
   }
 
-  deleteBook(val: BookDetails) {
+  addBook(book: BookDetails): void {
+
+    this.http.post<BookDetails>(this.api, book)
+  }
+
+
+  deleteBook(book: BookDetails) {
+    const url = `${this.api}/${book.id}`;
+
+    this.http.delete<BookDetails>(url)
 
   }
-  updateBook(val: BookDetails) {
+  updateBook(book: BookDetails) {
 
+    const url = `${this.api}/${book.id}`;
+    return this.http.put<BookDetails>(url, book)
   }
 
 
